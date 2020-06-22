9db8a305f1da6136ea4ad894f033444c3edd7f05
Francis Tsang <frtsang40@gmail.com>
2020-06-22 09:43:09 -0700

    users can create and submit

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+require('./controllers/posts.js')(app);
 
+// Set db
+require('./data/reddit-db');
+
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 app.get('/', (req, res) => {
     // res.send('Hello World!')
     res.render('index')
 });
+
+app.get('/posts/new', (req,res) =>{
+    res.render('posts-new')
+})
 
 // Middleware
 const exphbs  = require('express-handlebars');
