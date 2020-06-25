ca43b52d3b0e4fb0a75c2b8d0527aa94c2be3099
Francis Tsang <frtsang40@gmail.com>
2020-06-25 14:38:44 -0700

    users can see subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,41 +1,52 @@
 const Post = require('../models/post');
 
 module.exports = app => {
-   // CREATE
-    app.post('/posts/new', (req, res) => {
-        // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
-        
-        // post.toObject()
-        
-        // SAVE INSTANCE OF POST MODEL TO DB
-        post.save((err, post) => {
-        // REDIRECT TO THE ROOT
-        return res.redirect(`/`);
-        })
-    });
-   
-    // INDEX
-    app.get('/', (req, res) => {
-        Post.find({}).lean()
-            .then(posts => {
-                res.render("posts-index", { posts });
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
-    }); 
-   
-    //SHOW
-    app.get("/posts/:id", function(req, res) {
-        // LOOK UP THE POST
-        Post.findById(req.params.id) //.lean()
-          .then(post => {
-            res.render("posts-show", { "post" : post.toObject() });
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+      // INSTANTIATE INSTANCE OF POST MODEL
+      const post = new Post(req.body);
+      
+      // post.toObject()
+      
+      // SAVE INSTANCE OF POST MODEL TO DB
+      post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+      })
+  });
+  
+  // INDEX
+  app.get('/', (req, res) => {
+      Post.find({}).lean()
+          .then(posts => {
+              res.render("posts-index", { posts });
           })
           .catch(err => {
-            console.log(err.message);
+              console.log(err.message);
           });
+  }); 
+  
+  //SHOW
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id) //.lean()
+      .then(post => {
+        res.render("posts-show", { "post" : post.toObject() });
+      })
+      .catch(err => {
+        console.log(err.message);
       });
-    
+  });
+  
+  // Subreddits route
+  app.get("/n/:subreddit", function(req, res) {
+    Post.find({ subreddit: req.params.subreddit })
+      .then(posts => {
+        res.render("posts-index", { "posts" : posts.toObject });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
 };
