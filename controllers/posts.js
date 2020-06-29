ba61584a8c9509c020feac6dc528c612479823d3
Francis Tsang <frtsang40@gmail.com>
2020-06-29 13:49:47 -0700

    set up index and lobby routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,14 +1,28 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports = app => {
   // CREATE
-  app.post('/posts/new', (req, res) => {
+  app.post("/posts/new", (req, res) => {
     if (req.user) {
+    
       var post = new Post(req.body);
-  
-      post.save(function(err, post) {
-        return res.redirect(`/`);
-      });
+      post.author = req.user._id;
+
+      post
+        .save()
+        .then(post => {
+            return User.findById(req.user._id);
+        })
+        .then(user => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          res.redirect(`/posts/${post._id}`);
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
     } else {
       return res.status(401); // UNAUTHORIZED
     }
@@ -17,8 +31,10 @@ module.exports = app => {
   // INDEX
   app.get('/', (req, res) => {
     var currentUser = req.user;
-
+    console.log(req.cookies);
+    
     Post.find({})
+    .populate('author')
       .then(posts => {
         res.render("posts-index", { posts, currentUser });
       })
@@ -28,25 +44,29 @@ module.exports = app => {
   }); 
   
   //SHOW
-  app.get("/posts/:id", function(req, res) {
+  app.get("/posts/:id", function (req, res) {
+    var currentUser = req.user;
     // LOOK UP THE POST
-    Post.findById(req.params.id).populate('comments').then((post) => {
-      res.render('posts-show', { "post": post.toObject() })
-    }).catch((err) => {
-      console.log(err.message)
-    })
+
+    Post.findById(req.params.id).populate('comments').populate('author')
+        .then(post => {
+            res.render("posts-show", { post, currentUser });  
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
   });
   
   // SUBREDDIT
-  app.get("/n/:subreddit", function(req, res) {
-    Post.find({ subreddit: req.params.subreddit }).then(posts => {
-        posts = posts.map(function(posts) { return posts.toObject(); });
-        console.log(posts)
-        res.render("posts-index", { posts });
-    })
-    .catch(err => {
-        console.log(err);
-    });
+  app.get("/n/:subreddit", function (req, res) {
+    var currentUser = req.user;
+    Post.find({ subreddit: req.params.subreddit }).populate('author')
+        .then(posts => {
+            res.render("posts-index", { posts, currentUser });
+        })
+        .catch(err => {
+            console.log(err);
+        });
   });
   
 };
