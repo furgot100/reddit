efe8306d0a3bf55f4f8363a98451d95a98ee96d5
Francis Tsang <frtsang40@gmail.com>
2020-06-29 12:37:38 -0700

    user must login to create post

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,27 +3,28 @@ const Post = require('../models/post');
 module.exports = app => {
   // CREATE
   app.post('/posts/new', (req, res) => {
-      // INSTANTIATE INSTANCE OF POST MODEL
-      const post = new Post(req.body);
-      
-      // post.toObject()
-      
-      // SAVE INSTANCE OF POST MODEL TO DB
-      post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect(`/`);
-      })
+    if (req.user) {
+      var post = new Post(req.body);
+  
+      post.save(function(err, post) {
+        return res.redirect(`/`);
+      });
+    } else {
+      return res.status(401); // UNAUTHORIZED
+    }
   });
   
   // INDEX
   app.get('/', (req, res) => {
-      Post.find({}).lean()
-          .then(posts => {
-              res.render("posts-index", { posts });
-          })
-          .catch(err => {
-              console.log(err.message);
-          });
+    var currentUser = req.user;
+
+    Post.find({})
+      .then(posts => {
+        res.render("posts-index", { posts, currentUser });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
   }); 
   
   //SHOW
--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const port = 3000
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+      req.user = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, { complete: true }) || {};
+      req.user = decodedToken.payload;
+    }
+  
+    next();
+};
+
 
 
 // Use Body Parser
@@ -26,6 +39,8 @@ require('./data/reddit-db');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars'); 
 
+app.use(cookieParser()); // Add this after you initialize express.
+
 // app.get('/', (req, res) => {
 //     // res.send('Hello World!')
 //     res.render('index')
@@ -35,6 +50,7 @@ app.get('/posts/new', (req,res) =>{
     res.render('posts-new')
 })
 
+app.use(checkAuth);
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
